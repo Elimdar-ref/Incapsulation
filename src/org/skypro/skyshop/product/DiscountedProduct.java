f045a88a4194eddf15789c3b0907b42bc09fe231
Admin <elimdar.ref@mail.ru>
2025-05-21 01:00:29 +0300

    Incapsulation LP2

---
+++ src/org/skypro/skyshop/product/DiscountedProduct.java
@@ -0,0 +1,28 @@
+package org.skypro.skyshop.product;
+
+public class DiscountedProduct extends Product {
+
+    private int basePrice;
+    private double discount;
+
+    public DiscountedProduct(String productName, int basePrice, double discount) {
+        super(productName);
+        this.basePrice = basePrice;
+        this.discount = discount;
+    }
+
+    @Override
+    public int getPrice() {
+        return (int) (basePrice * (1 - discount / 100.00));
+    }
+
+    @Override
+    public String toString() {
+        return getProductName() + " со скидкой " + getPrice() + " : " + basePrice + " (скидка " + discount + "%)";
+        }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+}
